c3f4c09af1ec15eca7d3c4fcfea1d9967b6c6e73
gbarrbrown <gbb7899@gmail.com>
2019-04-10 00:09:27 +1200

    Add booleans example

---
+++ types.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	printBools()
+}
+
+func printBools() {
+	fmt.Println("-- Boolean -- ")
+	var isTrue bool = true
+	var isFalse bool = false
+	fmt.Println(isTrue)
+	fmt.Println(isFalse)
+}
